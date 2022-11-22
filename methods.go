b2f1de424fec6e7137af5920de0579ade43cdedb
Nitin Chauhan <chauhannitin.vn111@gmail.com>
2022-11-22 19:30:19 +0530

    Go methods with non struct

--- methods.go
+++ methods.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+//Mehods with struct
 type author struct {
 	name      string
 	branch    string
@@ -9,14 +10,23 @@ type author struct {
 	salary    int
 }
 
+// Methods wit non Struct
+type data int
+
 func main() {
-	res := author{
+	res1 := author{
 		name:      "Nitin Chauhan",
 		branch:    "CSE",
 		particles: 100,
 		salary:    35000,
 	}
-	res.show()
+	res1.show()
+
+	value1 := data(10)
+	value2 := data(13)
+
+	res2 := value1.multiply(value2)
+	fmt.Println("multiply of two number : ", res2)
 }
 
 func (a author) show() {
@@ -24,4 +34,8 @@ func (a author) show() {
 	fmt.Println("Branch Name: ", a.branch)
 	fmt.Println("Published articles: ", a.particles)
 	fmt.Println("Salary: ", a.salary)
+}
+
+func (d1 data) multiply(d2 data) data {
+	return d1 * d2
 }
