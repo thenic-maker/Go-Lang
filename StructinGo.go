58195578133c7263285a5d3d2ee36ef545ff2b66
Nitin Chauhan <chauhannitin.vn111@gmail.com>
2022-11-24 16:20:22 +0530

    nested structure implementation

--- StructinGo.go
+++ StructinGo.go
@@ -7,10 +7,33 @@ type employee struct {
 	salary int
 }
 
+// Creating structure
+type Student struct {
+	name   string
+	branch string
+	year   int
+}
+
+// Creating nested structure
+type Teacher struct {
+	name    string
+	subject string
+	exp     int
+	details Student
+}
+
 func main() {
 
 	a := &employee{"Nitin chauhan", 999999}
 
 	fmt.Println("Name: ", (*a).name)
 	fmt.Println("Salary: ", a.salary)
+
+	stu := Student{"Nitin Chauhan", "CSE", 2019}
+
+	teachr := Teacher{"Manoj", "C#", 10, stu}
+
+	fmt.Println("Student: ", stu)
+	fmt.Println("Teacher: ", teachr)
+
 }
