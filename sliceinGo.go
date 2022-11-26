536accca0861418f7fc072f84e234c19a1c8bebb
Nitin Chauhan <chauhannitin.vn111@gmail.com>
2022-11-26 19:10:15 +0530

    Sorting of slice using sort function

--- sliceinGo.go
+++ sliceinGo.go
@@ -1,12 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	//array
 	myarr := [5]string{"Hello ,", "This", "is", " Nitin", "Chauhan"}
 
 	fmt.Println("MyArr", myarr)
+
 	// slice
 	myslice := myarr[1:5]
 	fmt.Println("My Slice ", myslice)
@@ -32,4 +36,29 @@ func main() {
 	for _, ele1 := range myslice2 {
 		fmt.Printf("Element : %s \n", ele1)
 	}
+
+	// multi- dimensional slice
+	s1 := [][]int{{1, 2},
+		{3, 4},
+		{5, 6}}
+
+	fmt.Println("printing the s1 slice:", s1)
+
+	// this is the operation for the sorting
+	slc1 := []string{"s", "m", "l", "i", "a"}
+	slc2 := []int{45, 67, 23, 90, 33, 21, 56, 78, 89}
+
+	fmt.Println("Before sorting:")
+	fmt.Println("Slice 1: ", slc1)
+	fmt.Println("Slice 2: ", slc2)
+
+	// Performing sort operation on the
+	// slice using sort function
+	sort.Strings(slc1)
+	sort.Ints(slc2)
+
+	fmt.Println("\nAfter sorting:")
+	fmt.Println("Slice 1: ", slc1)
+	fmt.Println("Slice 2: ", slc2)
+
 }
