cdf6e33838962fbc903fec8b7a23a56596f91b12
Nitin Chauhan <chauhannitin.vn111@gmail.com>
2022-11-26 15:40:58 +0530

    Iterate using range

--- sliceinGo.go
+++ sliceinGo.go
@@ -14,8 +14,18 @@ func main() {
 	//make( []type , len ,cap)
 	var myslice1 = make([]int, 5, 10)
 
+	//Iterate slice
 	for i := 0; i < len(myslice1); i++ {
 		fmt.Scanf("%d", &myslice1[i])
 	}
 	fmt.Println("MY SLICE USING LOOP AND MAKE METHOD", myslice1)
+
+	myslice2 := []string{"This", "is", "the", "NITIN",
+		"CHAUHAN"}
+
+	// Iterate slice
+	// using range in for loop
+	for index, ele := range myslice2 {
+		fmt.Printf("Index = %d and element = %s\n", index, ele)
+	}
 }
